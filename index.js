f1631c35825b9c0129d132cfaaae540baad9672c
Jackline Jebet <jebetjackline2016@gmail.com>
2022-06-08 17:47:39 +0300

    Done

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Write your solution in this file!
+var customerName = 'bob'
+const leastCustomer = 'ray'
+
+function upperCaseCustomerName() {
+    window.customerName = customerName.toUpperCase()
+    return window.customerName;
+}
+
+function setBestCustomer() {
+    window.bestCustomer = 'not bob'
+}
+
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob'
+}
+
+function changeLeastFavoriteCustomer() {
+    leastCustomer = 'jacky'
+    return leastCustomer
+}
